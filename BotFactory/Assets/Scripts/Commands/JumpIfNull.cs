22aea2bc98593c2855de0b41bea0851888528f53
mpajak <marcin.paujak@euvic.pl>
2018-12-20 01:24:46 +0100

    Zmiany, zmiany

---
+++ BotFactory/Assets/Scripts/Commands/JumpIfNull.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Commands.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Commands
+{
+    class JumpIfNull : JumpCommand
+    {
+        public override string Description
+        {
+            get
+            {
+                return "Perform a program jump if certain memory cell is empty";
+            }
+        }
+
+        protected override bool DirectConditionCheck(Tank tank)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
